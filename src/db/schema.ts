be563ce4fcdb9814b62e0318aa771d1021a8ec5b
bouanimalek <malzovich@gmail.com>
2025-03-27 13:46:03 +0100

    feat: database setup (Neon/Drizzle ORM)

---
+++ src/db/schema.ts
@@ -0,0 +1,23 @@
+import {
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable(
+  "users",
+  {
+    id: uuid().primaryKey().defaultRandom(),
+    clerkId: text("clerk_id").unique().notNull(),
+    name: text("name").notNull(),
+    // TODO: Add banner fields
+    imageUrl: text("image_url").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)],
+);
